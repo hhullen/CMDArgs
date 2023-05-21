2bcde3c1e058c99335d2b6d6ffb4136051623562
go <go>
2023-05-21 22:24:07 +0700

    update: cmd_args.h class definition refactoring

--- src/cmd_args.h
+++ src/cmd_args.h
@@ -8,10 +8,11 @@
 
 namespace hhullen {
 
+using Str = std::string;
+
 class CMDArgs {
   using Argument = hhullen::Argument;
   using Flag = hhullen::Flag;
-  using Str = std::string;
   using StrStrPair = std::pair<Str, Str>;
   using Tokens = std::queue<Str>;
 
@@ -27,69 +28,14 @@ class CMDArgs {
   using ParsedFlagsIterator = ParsedFlags::iterator;
 
  public:
-  CMDArgs() {
-    class_flag_search_func = [this](const FlagsStructElement& element) {
-      return element.first.first == search_token_ ||
-             element.first.second == search_token_;
-    };
-    parsed_flag_search_func = [this](const ParsedFlagsElement& element) {
-      return element.first.first == search_token_ ||
-             element.first.second == search_token_;
-    };
-  }
-  ~CMDArgs() {}
+  CMDArgs();
+  ~CMDArgs();
 
-  void AddArguments(const std::initializer_list<Argument>& args) {
-    for (Argument arg : args) {
-      arguments_.push(arg);
-    }
-  }
-
-  void AddFlags(const std::initializer_list<Flag>& flags) {
-    for (Flag flag : flags) {
-      Str name_long = Str("--") + flag.GetLongName();
-      Str name_short = Str("-") + flag.GetShortName();
-      flags_.insert({{name_long, name_short}, flag});
-    }
-  }
-
-  Str GetArgument(const Str& name) {
-    if (!IsArgExists(name)) {
-      throw std::invalid_argument("\"" + name + "\" was not specified.");
-    }
-    return positional_[name];
-  }
-
-  std::list<Str> GetFlagValues(const Str& name) {
-    search_token_ = name;
-    ParsedFlagsIterator iter = std::find_if(optional_.begin(), optional_.end(),
-                                            parsed_flag_search_func);
-    search_token_.clear();
-    if (iter == optional_.end()) {
-      throw std::invalid_argument("\"" + name + " was not specified.");
-    }
-    return (*iter).second;
-  }
-
-  void Read(int argc, const char* argv[]) {
-    CopyToThis(argc, argv);
-    for (; !tokens_.empty();) {
-      Str token = tokens_.front();
-      if (Argument::IsArgument(token) && !arguments_.empty()) {
-        Argument argument = arguments_.front();
-        ReadArgumentFromToken(argument, token);
-        arguments_.pop();
-        tokens_.pop();
-      } else if (Flag::IsFlag(token)) {
-        Flag flag = GetFlagFromToken(token);
-        ReadFlag(flag);
-      } else {
-        throw std::invalid_argument("Unknown argument \"" + token +
-                                    "\" specified.");
-      }
-    }
-    CheckRemainsArguments();
-  }
+  void AddArguments(const std::initializer_list<Argument>& args);
+  void AddFlags(const std::initializer_list<Flag>& flags);
+  Str GetArgument(const Str& name);
+  std::list<Str> GetFlagValues(const Str& name);
+  void Read(int argc, const char* argv[]);
 
  private:
   ParsedArguments positional_;
@@ -104,83 +50,156 @@ class CMDArgs {
   std::function<bool(const FlagsStructElement&)> class_flag_search_func;
   std::function<bool(const ParsedFlagsElement&)> parsed_flag_search_func;
 
-  void CopyToThis(int argc, const char* argv[]) {
-    for (int i = 1; i < argc; ++i) {
-      tokens_.push(argv[i]);
-    }
+  void CopyToThis(int argc, const char* argv[]);
+  bool IsArgExists(const Str& name);
+  Flag GetFlagFromToken(const Str& token);
+  void ReadFlag(Flag& flag);
+  void CheckFlagValuesAbsence(Flag& flag);
+  void SetParsedValueForFlag(const Str& value, Flag& flag);
+  void ReadArgumentFromToken(Argument& argument, const Str& token);
+  void CheckRemainsArguments();
+};
+
+CMDArgs::CMDArgs() {
+  class_flag_search_func = [this](const FlagsStructElement& element) {
+    return element.first.first == search_token_ ||
+           element.first.second == search_token_;
+  };
+  parsed_flag_search_func = [this](const ParsedFlagsElement& element) {
+    return element.first.first == search_token_ ||
+           element.first.second == search_token_;
+  };
+}
+CMDArgs::~CMDArgs() {}
+
+void CMDArgs::AddArguments(const std::initializer_list<Argument>& args) {
+  for (Argument arg : args) {
+    arguments_.push(arg);
   }
+}
 
-  bool IsArgExists(const Str& name) {
-    return positional_.find(name) != positional_.end();
-  }
-
-  Flag GetFlagFromToken(const Str& token) {
-    search_token_ = token;
-    FlagsStructIterator iter =
-        std::find_if(flags_.begin(), flags_.end(), class_flag_search_func);
-    search_token_.clear();
-
-    if (iter == flags_.end()) {
-      throw std::invalid_argument("Unknown flag \"" + token + "\" specified.");
-    }
-    return (*iter).second;
-  }
-
-  void ReadFlag(Flag& flag) {
-    const std::list<Argument>& arguments = flag.GetArguments();
-    ArgumentsListIterator iter = arguments.begin();
-    tokens_.pop();
-
-    CheckFlagValuesAbsence(flag);
-    for (; !tokens_.empty() && iter != arguments.end(); tokens_.pop()) {
-      Argument argument = *iter;
-      Str token = tokens_.front();
-      argument.ReadArgument(token);
-
-      Str value = argument.GetValue();
-      SetParsedValueForFlag(value, flag);
-      ++iter;
-    }
-
-    if (iter != arguments.end()) {
-      Argument argument = *iter;
-      throw std::invalid_argument("Value \"" + argument.GetName() +
-                                  "\" of flag [--" + flag.GetLongName() + " -" +
-                                  flag.GetShortName() + "] was not specified.");
-    }
-  }
-
-  void CheckFlagValuesAbsence(Flag& flag) {
-    const std::list<Argument>& arguments = flag.GetArguments();
-    if (arguments.empty()) {
-      SetParsedValueForFlag("true", flag);
-    }
-  }
-
-  void SetParsedValueForFlag(const Str& value, Flag& flag) {
+void CMDArgs::AddFlags(const std::initializer_list<Flag>& flags) {
+  for (Flag flag : flags) {
     Str name_long = Str("--") + flag.GetLongName();
     Str name_short = Str("-") + flag.GetShortName();
-    // CheckNameUniquenessInContainer(name, optional_); !!!!!!!
-    optional_[{name_long, name_short}].push_back(value);
+    flags_.insert({{name_long, name_short}, flag});
   }
+}
 
-  void ReadArgumentFromToken(Argument& argument, const Str& token) {
+Str CMDArgs::GetArgument(const Str& name) {
+  if (!IsArgExists(name)) {
+    throw std::invalid_argument("\"" + name + "\" was not specified.");
+  }
+  return positional_[name];
+}
+
+std::list<Str> CMDArgs::GetFlagValues(const Str& name) {
+  search_token_ = name;
+  ParsedFlagsIterator iter =
+      std::find_if(optional_.begin(), optional_.end(), parsed_flag_search_func);
+  search_token_.clear();
+  if (iter == optional_.end()) {
+    throw std::invalid_argument("\"" + name + " was not specified.");
+  }
+  return (*iter).second;
+}
+
+void CMDArgs::Read(int argc, const char* argv[]) {
+  CopyToThis(argc, argv);
+  for (; !tokens_.empty();) {
+    Str token = tokens_.front();
+    if (Argument::IsArgument(token) && !arguments_.empty()) {
+      Argument argument = arguments_.front();
+      ReadArgumentFromToken(argument, token);
+      arguments_.pop();
+      tokens_.pop();
+    } else if (Flag::IsFlag(token)) {
+      Flag flag = GetFlagFromToken(token);
+      ReadFlag(flag);
+    } else {
+      throw std::invalid_argument("Unknown argument \"" + token +
+                                  "\" specified.");
+    }
+  }
+  CheckRemainsArguments();
+}
+
+void CMDArgs::CopyToThis(int argc, const char* argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    tokens_.push(argv[i]);
+  }
+}
+
+bool CMDArgs::IsArgExists(const Str& name) {
+  return positional_.find(name) != positional_.end();
+}
+
+Flag CMDArgs::GetFlagFromToken(const Str& token) {
+  search_token_ = token;
+  FlagsStructIterator iter =
+      std::find_if(flags_.begin(), flags_.end(), class_flag_search_func);
+  search_token_.clear();
+
+  if (iter == flags_.end()) {
+    throw std::invalid_argument("Unknown flag \"" + token + "\"specified.");
+  }
+  return (*iter).second;
+}
+
+void CMDArgs::ReadFlag(Flag& flag) {
+  const std::list<Argument>& arguments = flag.GetArguments();
+  ArgumentsListIterator iter = arguments.begin();
+  tokens_.pop();
+
+  CheckFlagValuesAbsence(flag);
+  for (; !tokens_.empty() && iter != arguments.end(); tokens_.pop()) {
+    Argument argument = *iter;
+    Str token = tokens_.front();
     argument.ReadArgument(token);
+
     Str value = argument.GetValue();
-    Str name = argument.GetName();
-    if (positional_.find(name) != positional_.end()) {
-      throw std::invalid_argument("Name \"" + name + "\" already read.");
-    }
-    positional_[name] = value;
+    SetParsedValueForFlag(value, flag);
+    ++iter;
   }
 
-  void CheckRemainsArguments() {
-    if (!arguments_.empty()) {
-      Str name = arguments_.front().GetName();
-      throw std::invalid_argument("\"" + name + " was not specified.");
-    }
+  if (iter != arguments.end()) {
+    Argument argument = *iter;
+    throw std::invalid_argument("Value \"" + argument.GetName() +
+                                "\" of flag [--" + flag.GetLongName() + "-" +
+                                flag.GetShortName() + "] was not specified.");
   }
-};
+}
+
+void CMDArgs::CheckFlagValuesAbsence(Flag& flag) {
+  const std::list<Argument>& arguments = flag.GetArguments();
+  if (arguments.empty()) {
+    SetParsedValueForFlag("true", flag);
+  }
+}
+
+void CMDArgs::SetParsedValueForFlag(const Str& value, Flag& flag) {
+  Str name_long = Str("--") + flag.GetLongName();
+  Str name_short = Str("-") + flag.GetShortName();
+  // CheckNameUniquenessInContainer(name, optional_); !!!!!!!
+  optional_[{name_long, name_short}].push_back(value);
+}
+
+void CMDArgs::ReadArgumentFromToken(Argument& argument, const Str& token) {
+  argument.ReadArgument(token);
+  Str value = argument.GetValue();
+  Str name = argument.GetName();
+  if (positional_.find(name) != positional_.end()) {
+    throw std::invalid_argument("Name \"" + name + "\" already read.");
+  }
+  positional_[name] = value;
+}
+
+void CMDArgs::CheckRemainsArguments() {
+  if (!arguments_.empty()) {
+    Str name = arguments_.front().GetName();
+    throw std::invalid_argument("\"" + name + " was not specified.");
+  }
+}
 
 }  // namespace hhullen
 
